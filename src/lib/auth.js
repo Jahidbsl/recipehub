624bff7dd14ed6e2dc200181fba1c1412934c7e2
jahid <jahidhasanbsl46@gmail.com>
2026-06-24 18:43:44 +0600

    update social provider

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,5 @@
 import { betterAuth } from "better-auth";
-import { MongoClient, ObjectId } from "mongodb"; 
+import { MongoClient, ObjectId } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { admin } from "better-auth/plugins";
 
@@ -8,28 +8,34 @@ const db = client.db("recipehubDb");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, { client }),
-  
+
   emailAndPassword: {
     enabled: true,
     onSignUp: async ({ user }) => {
       user.role = "user";
     },
   },
-  
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+
   plugins: [admin()],
 
   user: {
     additionalFields: {
       plan: {
         type: "string",
-        defaultValue: "free", 
+        defaultValue: "free",
       },
       isBlocked: {
         type: "boolean",
         defaultValue: false,
-      }
+      },
     },
-  }, 
+  },
 
   // 🔐 সার্ভার-সাইড হুক (নিখুঁত ও ডাইনামিক চেক)
   hooks: {
@@ -37,15 +43,19 @@ export const auth = betterAuth({
       user: {
         signIn: {
           before: async (user) => {
-            // 💡 Better Auth অনেক সময় ক্যাশ ডেটা ব্যবহার করে। 
+            // 💡 Better Auth অনেক সময় ক্যাশ ডেটা ব্যবহার করে।
             // তাই আমরা সরাসরি মঙ্গোডিবি থেকে ইউজারের একদম লেটেস্ট ডেটা তুলে এনে চেক করব।
             // আপনার কালেকশনের নাম 'user' হলে 'user' দিবেন, 'users' হলে 'users' দিবেন।
-            const freshUser = await db.collection("user").findOne({ 
-              _id: typeof user.id === "string" ? new ObjectId(user.id) : user._id 
+            const freshUser = await db.collection("user").findOne({
+              _id:
+                typeof user.id === "string" ? new ObjectId(user.id) : user._id,
             });
 
             // 🛑 মঙ্গোডিবি থেকে পাওয়া তাজা ডেটায় যদি 'isBlocked' ট্রু (true) থাকে
-            if (freshUser && (freshUser.isBlocked === true || freshUser.isBlocked === "true")) {
+            if (
+              freshUser &&
+              (freshUser.isBlocked === true || freshUser.isBlocked === "true")
+            ) {
               throw new Error("Your account has been blocked by the admin! 🚫");
             }
 
