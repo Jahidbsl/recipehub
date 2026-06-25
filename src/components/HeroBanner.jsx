64aabf87a062701a8b91761f41fbb430efa059e7
jahid <jahidhasanbsl46@gmail.com>
2026-06-25 18:35:45 +0600

    update banner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -86,7 +86,7 @@ export default function HeroBanner() {
               </motion.button>
             </Link>
 
-            <Link href="/dashboard" className="w-full sm:w-auto">
+            <Link href="/dashboard/add" className="w-full sm:w-auto">
               <motion.button
                 whileHover={{ scale: 1.03 }}
                 whileTap={{ scale: 0.98 }}
